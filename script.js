035deca0c1ef2032484e395b189375c34cd64141
nathaliaValente1 <nathalia.gomes.ferreira@escola.pr.gov.br>
2026-08-14 08:51:43 -0300

    ok

---
+++ script.js
@@ -0,0 +1,41 @@
+//Botões de acessibilidade
+document.addEventListener("DOMContentLoaded, () =>{
+    
+    const btnContraste = document.getElementById("btn-contraste");
+    const btnAumentar = document.getElementById("btn-aumentar-texto");
+    const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+    //aumenta a fonte 
+
+    let tamanhoAtualFonte = 100;
+
+    //função alto contraste
+
+btnConstraste.addEventListener("click", () => {
+
+document.body.classList.toogle("Alto-contraste");
+
+//acessibilidade para leitores de template
+
+const ativo = document.body. classList.contains("alto-contraste");
+btnContraste.setAttribute("aria-pressed", ativo);
+
+
+});
+//função aumentar texto
+
+btnAumentar.addEventListener("click", ()=>{
+    if (tamanhoAtualFonte < 150){
+        tamanhoAtualFonte += 10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+    }
+})
+
+//função diminuir texto
+
+btnDiminuir.addEventListener("click", ()=> {
+    if(tamanhoAtualFonte > 90){
+        tamanhoAtualFonte -=10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+    }
+})
